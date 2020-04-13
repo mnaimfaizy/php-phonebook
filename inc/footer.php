5a4f6ba7889dec9389aaaa51871b2ea700148be3
Mohammad Naim Faizy <mnaim.faizy@gmail.com>
2020-04-13 20:20:48 +0430

    add calendar widget to dashboard

--- inc/footer.php
+++ inc/footer.php
@@ -8,6 +8,40 @@
 <script src="assets/js/bootstrap.bundle.min.js"></script>
 <script src="assets/js/dashboard.js"></script>
 
+<?php // We will check if the page is index.php and load the script
+      if(basename($_SERVER['PHP_SELF']) === 'index.php') { ?>
+    <!-- EVO Calendar Script -->
+    <script src="assets/event-calendar-evo/evo-calendar.js"></script>
+    <script>
+        $('#evoCalendar').evoCalendar({
+            todayHighlight: true,
+            sidebarToggler: true,
+            eventListToggler: true,
+            eventDisplayDefault: false,
+            canAddEvent: true,
+            calendarEvents: [
+                { name: "New Year", date: "Wed Jan 01 2020 00:00:00 GMT-0800 (Pacific Standard Time)", type: "holiday", everyYear: true },
+                { name: "Valentine's Day", date: "Fri Feb 14 2020 00:00:00 GMT-0800 (Pacific Standard Time)", type: "holiday", everyYear: true },
+                { name: "Patient #1", date: "February/3/2020", type: "event" },
+                { name: "Patient #3", date: "February/3/2020", type: "event" },
+                { name: "Patient #4", date: "February/3/2020", type: "event" },
+                { name: "Holiday #3", date: "February/3/2020", type: "holiday" },
+                { name: "Birthday #2", date: "February/3/2020", type: "birthday" },
+                { name: "Author's Birthday", date: "February/15/2020", type: "birthday", everyYear: true },
+                { name: "Holiday #4", date: "February/15/2020", type: "event" },
+                { name: "Patient #2", date: "February/8/2020", type: "event" },
+                { name: "Leap day", date: "February/29/2020", type: "holiday", everyYear: true }
+            ],
+            onSelectDate: function() {
+                console.log('onSelectDate!')
+            },
+            onAddEvent: function() {
+                console.log('onAddEvent!')
+            }
+        });
+    </script>
+<?php } ?>
+
 <!-- script-for-menu -->
 <script>
     $( "span.menu-info" ).click(function() {
--- index.php
+++ index.php
@@ -1,53 +1,55 @@
 <?php include('inc/header.php'); ?>
 <?php include('inc/nav.php'); ?>
 
-<main role="main" class="container">
+<main role="main" class="container-fluid">
 
 <?php include('inc/breadcrumb.php'); ?>
 
 <div class="my-3 p-3 bg-white rounded shadow-sm">
     <div class="row">
-        <div class="col-sm-12 col-md-8">
+        <div class="col-sm-12 col-md-2">
             <!-- circles -->
             <div class="circles">
                 <h3>Status</h3>
-                <div class="table-responsive">
-                    <table class="table">
-                        <tr>
-                            <td width="50%" style="padding: 0px 30px;">
-                                <div id="advanced" class="circle">
-                                    <?php $files = glob('users/*.xml');
-                                    if (count($files) > 0) {
-                                        echo count($files);
-                                    } else {
-                                        echo 0;
-                                    }
-                                    ?>
-                                </div>
-                            </td>
-                            <td width="50%" style="padding: 0px 30px;">
-                                <div id="advanced" class="circle">
-                                    <?php $files = glob('numbers/*.xml');
-                                    if (count($files) > 0) {
-                                        echo count($files);
-                                    } else {
-                                        echo 0;
-                                    }
-                                    ?>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td><h3>Users</h3></td>
-                            <td><h3>Contacts</h3></td>
-                        </tr>
-                    </table>
+                <div class="card text-center mb-3">
+                    <div class="card-header">
+                        Users
+                    </div>
+                    <div class="card-body">
+                        <div id="advanced" class="circle">
+                            <?php $files = glob('users/*.xml');
+                            if (count($files) > 0) {
+                                echo count($files);
+                            } else {
+                                echo 0;
+                            }
+                            ?>
+                        </div>
+                    </div>
                 </div>
+
+                <div class="card text-center mb-3">
+                    <div class="card-header">
+                        Contacts
+                    </div>
+                    <div class="card-body">
+                        <div id="advanced" class="circle">
+                            <?php $files = glob('numbers/*.xml');
+                            if (count($files) > 0) {
+                                echo count($files);
+                            } else {
+                                echo 0;
+                            }
+                            ?>
+                        </div>
+                    </div>
+                </div>
+
             </div>
             <!-- //circles -->
         </div>
-        <div class="col-sm-12 col-md-4">
-            <?php include('inc/users_list.php'); ?>
+        <div class="col-sm-12 col-md-10">
+            <div id="evoCalendar"></div>
         </div>
     </div>
 </div>
